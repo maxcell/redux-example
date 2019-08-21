81c2a0e443db54e32c04621c35fa2e4a6ac63709
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-08-21 12:15:29 -0400

    Adds initial setup for the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    likes: 0,
+    text: ''
+  }
+
+  like = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1
+    }))
+  }
+
+  dislike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes - 1
+    }))
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <h4>{this.state.likes} likes!</h4>
+        <button onClick={this.like}><span role="img" aria-label="thumbs up">👍</span></button>
+        <button onClick={this.dislike}><span role="img" aria-label="thumbs down">👎</span></button>
+        <input type="text" value={this.state.text} onChange={this.handleChange} />
+        <p>{this.state.text}</p>
+      </div>
+    )
+
+  }
 }
 
 export default App;
